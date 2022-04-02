1196d2608155ea93458c71725854584e95b593ba
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2022-04-03 00:44:31 +0200

    changed club page displaying

--- src/Form/ClubType.php
+++ src/Form/ClubType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Club;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,9 @@ class ClubType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('city')
-            ->add('country')
+            ->add('name', TextType::class, ['label' => 'Name '])
+            ->add('city', TextType::class, ['label' => 'City '])
+            ->add('country', TextType::class, ['label' => 'Country '])
         ;
     }
 
