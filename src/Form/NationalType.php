562a639e38e198c30960a501e28a102034a2654b
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2022-04-05 18:10:07 +0200

    valid url checking added

--- src/Form/NationalType.php
+++ src/Form/NationalType.php
@@ -7,15 +7,28 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class NationalType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('flag', TextType::class, ['label' => 'Flag icon url '])
-            ->add('name', TextType::class, ['label' => 'Name '])
-            ->add('country', TextType::class, ['label' => 'Representated country '])
+            ->add('flag', TextType::class, ['label' => 'Flag icon url ', 'attr' => array('placeholder' => 'flag'),
+            'constraints' => [new Assert\Callback([
+                'callback' => static function(?string $value, ExecutionContextInterface $context){
+                    if(!str_ends_with($value, ".png")){
+                        $context
+                            ->buildViolation('Url must end by ".png"')
+                            ->atPath('[flag]')
+                            ->addViolation();
+                    }
+                }
+            ]
+            )]])
+            ->add('name', TextType::class, ['label' => 'Name ', 'attr' => array('placeholder' => 'name')])
+            ->add('country', TextType::class, ['label' => 'Representated country ', 'attr' => array('placeholder' => 'country')])
         ;
     }
 
--- src/Form/PlayerType.php
+++ src/Form/PlayerType.php
@@ -11,20 +11,33 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class PlayerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('photo', TextType::class, ['label' => 'Image url '])
-            ->add('firstname', TextType::class, ['label' => 'First name '])
-            ->add('lastname', TextType::class, ['label' => 'Last name '])
-            ->add('age', IntegerType::class, ['label' => 'Age '])
-            ->add('size', IntegerType::class, ['label' => 'Size (cm) '])
-            ->add('nationality', TextType::class, ['label' => 'Country birth (put 3 first letters or official initials) '])
-            ->add('club', EntityType::class, ['class' => Club::class, 'label' => 'Current club ', 'choice_label' => 'name'])
-            ->add('national', EntityType::class, ['class' => National::class, 'label' => 'Country choosen ', 'choice_label' => 'name'])
+            ->add('photo', TextType::class, ['label' => 'Image url ', 'attr' => array('placeholder' => 'url/pic.png'),
+            'constraints' => [new Assert\Callback([
+                'callback' => static function(?string $value, ExecutionContextInterface $context){
+                    if(!str_ends_with($value, ".png")){
+                        $context
+                            ->buildViolation('Url must end by ".png"')
+                            ->atPath('[photo]')
+                            ->addViolation();
+                    }
+                }
+            ]
+            )]])
+            ->add('firstname', TextType::class, ['label' => 'First name ', 'attr' => array('placeholder' => 'firstname')])
+            ->add('lastname', TextType::class, ['label' => 'Last name ', 'attr' => array('placeholder' => 'lastname')])
+            ->add('age', IntegerType::class, ['label' => 'Age ', 'attr' => array('placeholder' => 'age')])
+            ->add('size', IntegerType::class, ['label' => 'Size (cm) ', 'attr' => array('placeholder' => 'size')])
+            ->add('nationality', TextType::class, ['label' => 'Country birth (put 3 first letters or official initials) ', 'attr' => array('placeholder' => 'nat')])
+            ->add('club', EntityType::class, ['class' => Club::class, 'label' => 'Current club ', 'choice_label' => 'name', 'attr' => array('placeholder' => 'club')])
+            ->add('national', EntityType::class, ['class' => National::class, 'label' => 'Country choosen ', 'choice_label' => 'name', 'attr' => array('placeholder' => 'country')])
         ;
     }
 
